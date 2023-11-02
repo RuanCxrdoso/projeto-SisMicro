8d78d34028f85240547d38db1f10f4d7c2e7c6c7
Ruan Cardoso dos Santos <cardosoruan2001@gmail.com>
2023-11-02 20:50:58 -0300

    feat: Initial Project

---
+++ projetoRobo/main.cpp
@@ -0,0 +1,22 @@
+/*
+	projetoRobo.cpp
+ 
+	Created: 02/11/2023 18:35:41
+	Author : Ruan Cardoso, Mikelly Correia, Edilton Damasceno e Kelmer
+	Theme: Instrumentation of a differential robot
+	Micro controller: ATMega328P
+	Clock: 16Mhz
+	
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+

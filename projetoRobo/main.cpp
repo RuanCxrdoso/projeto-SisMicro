41be453b6a9507bc95ff7cbbfad5dee1feae64e8
Ruan Cardoso dos Santos <cardosoruan2001@gmail.com>
2023-11-02 21:26:41 -0300

    fix: Change author name

--- projetoRobo/main.cpp
+++ projetoRobo/main.cpp
@@ -2,7 +2,7 @@
 	projetoRobo.cpp
  
 	Created: 02/11/2023 18:35:41
-	Author : Ruan Cardoso, Mikelly Correia, Edilton Damasceno e Kelmer
+	Author : Ruan Cardoso, Mikelly Correia, Edilton Damasceno e Kelmer Passos
 	Theme: Instrumentation of a differential robot
 	Micro controller: ATMega328P
 	Clock: 16Mhz

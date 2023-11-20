5d8ef95c3acbe719c3020eb563ad57314db7fb13
Ruan Cardoso dos Santos <cardosoruan2001@gmail.com>
2023-11-20 16:11:48 -0300

    docs: Documentação de algumas linhas

--- projetoRobo/codigo_robo.c
+++ projetoRobo/codigo_robo.c
@@ -2,47 +2,47 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
-#define F_CPU 16000000UL
-#define TRIGGER_PIN 3
-#define ECHO_PIN 2
+#define F_CPU 16000000UL // Frequencia de clock do ATmega328P 16mHz
+#define TRIG_PIN 3 // Trigger do sensor - PD3 (emissor do pulso)
+#define ECHO_PIN 2 // Echo do sensor - PD2 (receptor do pulso)
 
-#define IN1 7
-#define IN2 6
-#define IN3 5
-#define IN4 4
+#define IN1 7 // pino de controle da ponte H (PD7)
+#define IN2 6 // pino de controle da ponte H (PD6)
+#define IN3 5 // pino de controle da ponte H (PD5)
+#define IN4 4 // pino de controle da ponte H (PD4)
 
-#define ENA 9
-#define ENB 10
+#define ENA 9 // Pinos de controle de velocidade usando o PWM para motor esquerdo (PB1)
+#define ENB 10 // Pinos de controle de velocidade usando o PWM para motor direito (PB2)
 
-#define TRIG_PIN PD2
-#define ECHO_PIN PD3
+// #define TRIG_PIN TRIGGER_PIN 
+// #define ECHO_PIN ECHO_PIN 
 
-volatile uint16_t pulse_width = 0;
+volatile uint16_t pulse_width = 0; // vari√°vel vol√°til (pode ter seu valor alterado a qualquer momento no programa) para largura de pulso
 
-void setup() {
-	// ConfiguraÁ„o dos pinos dos motores
-	DDRD |= (1 << IN1) | (1 << IN2) | (1 << IN3) | (1 << IN4);
-	// ConfiguraÁ„o dos pinos de controle de velocidade
-	DDRD |= (1 << ENA) | (1 << ENB);
+void setup() { // Configura√ß√µes iniciais
+	// Configura√ß√£o dos pinos dos motores
+	DDRD |= (1 << IN1) | (1 << IN2) | (1 << IN3) | (1 << IN4); // Seta todos os 4 pinos de controle da ponte H como sa√≠da (output)
+	// Configura√ß√£o dos pinos de controle de velocidade
+	DDRD |= (1 << ENA) | (1 << ENB); // Seta ambos os pinos como sa√≠da (output)
 
 	init_PWM();
 	set_PWM(120, 120);
 
-	// ConfiguraÁ„o dos pinos de trigger (saÌda) e echo (entrada)
+	// Configura√ß√£o dos pinos de trigger (sa√≠da) e echo (entrada)
 	DDRD |= (1 << TRIG_PIN);
 	DDRD &= ~(1 << ECHO_PIN);
 
 	init_timer1();
-	sei(); // Habilita interrupÁıes globais
+	sei(); // Habilita interrup√ß√µes globais
 }
 
 void loop() {
-	// RobÙ inicia andando para frente;
+	// Rob√¥ inicia andando para frente;
 	robo_frente();
 
-	float dist_cm = calculate_distance(); // Declara vari·vel que armazena a dist‚ncia do obst·culo;
+	float dist_cm = calculate_distance(); // Declara vari√°vel que armazena a dist√¢ncia do obst√°culo;
 
-	if (dist_cm < 20) // dist‚ncia menor que 20cm?;
+	if (dist_cm < 20) // dist√¢ncia menor que 20cm?;
 	{
 		decisao();
 	}
@@ -50,28 +50,30 @@ void loop() {
 	_delay_ms(100);
 }
 
+// Fun√ß√µes auxiliares
+
 void init_PWM() {
-	// ConfiguraÁ„o do Timer/Counter1 para PWM;
-	TCCR1A |= (1 << COM1A1) | (1 << COM1B1) | (1 << WGM10);
-	TCCR1B |= (1 << CS11) | (1 << WGM12);
-	// ConfiguraÁ„o dos pinos de saÌda para os canais A e B;
+	// Configura√ß√£o do Timer/Counter1 para PWM;
+	TCCR1A |= (1 << COM1A1) | (1 << COM1B1) | (1 << WGM10); // COM1A1 COM1B1 configurado para limpar OC1A e OC1B quando a compara√ß√£o da um 'match' durante a contagem.
+	TCCR1B |= (1 << CS11) | (1 << WGM12); // CS11 configura o prescaler para 1:8 // WGM10 E WGM12 configuram o modo de opera√ß√£o do Timer/counter como modo 5, fast PWM de 8 bits.
+	// Configura√ß√£o dos pinos de sa√≠da para os canais A e B;
 	DDRB |= (1 << ENA) | (1 << ENB);
 }
 
-void set_PWM(uint8_t duty_cycle_A, uint8_t duty_cycle_B) {
-	// Configurao do ciclo de trabalho para os canais A e B;
-	OCR1A = duty_cycle_A;
-	OCR1B = duty_cycle_B;
+void set_PWM(uint8_t duty_cycle_A, uint8_t duty_cycle_B) { // valor como (128, 128) indica 50% de duty cicle em ambos os canais
+	// Configura√ß√£o do ciclo de trabalho para os canais A e B;
+	OCR1A = duty_cycle_A; // ciclo de trabalho (clock em n√≠vel alto) de 0 a 255
+	OCR1B = duty_cycle_B; // ciclo de trabalho (clock em n√≠vel alto) de 0 a 255
 }
 
 void init_timer1() {
 	// Configura o Timer1 para capturar o tempo de pulso;
-	TCCR1B |= (1 << ICES1); // Configura para captura no flanco de subida (rising edge);
-	TIMSK1 |= (1 << ICIE1); // Ativa a interrupÁ„o de captura;
+	TCCR1B |= (1 << ICES1); // Configura√ß√£o para captura na borda de subida (rising edge);
+	TIMSK1 |= (1 << ICIE1); // Ativa a interrup√ß√£o quando ocorre a captura de pulso;
 }
 
 void send_pulse() {
-	// Gera um pulso de 10 microssegundos no pino de trigger;
+	// Gera um pulso de 10 microssegundos no pino de trigger do sensor ultrass√¥nico;
 	PORTD |= (1 << TRIG_PIN);
 	_delay_us(10);
 	PORTD &= ~(1 << TRIG_PIN);
@@ -80,9 +82,9 @@ void send_pulse() {
 float calculate_distance() {
 	// Velocidade do som no ar a 20 graus Celsius (em metros por segundo);
 	float speed_of_sound = 343.0;
-	// Fator de convers„o para transformar a largura do pulso em centÌmetros;
-	float conversion_factor = 100.0 / 2.0;  // Supondo que a largura do pulso est· em microssegundos;
-	// Calcula a dist‚ncia usando a fÛrmula;
+	// Fator de convers√£o para transformar a largura do pulso em cent√≠metros;
+	float conversion_factor = 100.0 / 2.0;  // Supondo que a largura do pulso est√° em microssegundos;
+	// Calcula a dist√¢ncia usando a f√≥rmula;
 	return (pulse_width / 2.0) * (speed_of_sound / 1000000.0) * conversion_factor;
 }
 
@@ -100,6 +102,13 @@ void robo_esquerda() {
 	PORTD |= (1 << IN4);
 }
 
+void robo_direita() {
+	PORTD &= (1 << IN1);
+	PORTD |= ~(1 << IN2);
+	PORTD |= ~(1 << IN3);
+	PORTD &= (1 << IN4);
+}
+
 void robo_parado() {
 	PORTD &= ~(1 << IN1);
 	PORTD &= ~(1 << IN2);
@@ -111,23 +120,23 @@ void decisao() {
 	robo_parado();
 	_delay_ms(500);
 	robo_esquerda();
-	// Esse tempo precisa ser avaliado para que o robÙ vire por volta de 90 graus;
+	// Esse tempo precisa ser avaliado para que o rob√¥ vire por volta de 90 graus;
 	_delay_ms(400);
 	robo_parado();
 	_delay_ms(500);
 }
 
 ISR(TIMER1_CAPT_vect) {
-	// InterrupÁ„o È acionada quando o pulso È capturado;
+	// Interrup√ß√£o √© acionada quando o pulso √© capturado;
 	static uint16_t start_time = 0;
 
 	if (TCCR1B & (1 << ICES1)) {
-		// Flanco de subida detectado;
-		start_time = ICR1; // Salva o tempo de inÌcio do pulso;
+		// borda de subida detectado;
+		start_time = ICR1; // Salva o tempo de in√≠cio do pulso;
 		TCCR1B &= ~(1 << ICES1); // Muda para captura no flanco de descida;
 		} else {
 		// Flanco de descida detectado;
-		uint16_t end_time = ICR1; // Salva o tempo de tÈrmino do pulso;
+		uint16_t end_time = ICR1; // Salva o tempo de t√©rmino do pulso;
 		pulse_width = end_time - start_time; // Calcula a largura do pulso em microssegundos;
 		TCCR1B |= (1 << ICES1); // Muda de volta para captura no flanco de subida;
 	}

bc41ffacef1196ab27e49d99792db6f487e96c6b
puacorreia <135282553+puacorreia@users.noreply.github.com>
2023-12-03 22:06:21 -0300

    Create adc.c

---
+++ adc.c
@@ -0,0 +1,32 @@
+ISR(TIMER0_COMPA_vect) {
+    count++;
+    if (count == 100) { 
+        PORTD ^= (1 << PORTD0); 
+        count = 0;
+    }
+}
+
+int main(void) {
+    DDRD |= (1 << DDD6); 
+
+    // ADC Configuration - Using ADC0 (Pin A0)
+    ADMUX = 0; // Use ADC0
+    ADMUX |= (1 << REFS0); // AVCC as voltage reference
+    ADCSRA |= (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); 
+
+    // Timer0 Configuration for Fast PWM
+    TCCR0A = (1 << COM0A1) | (1 << WGM00) | (1 << WGM01); 
+    TCCR0B = (1 << CS00); 
+
+    while (1) {
+        ADCSRA |= (1 << ADSC); 
+        while (ADCSRA & (1 << ADSC)); 
+        
+        OCR0A = ADC >> 2; 
+        PORTB = ADC;
+ 
+ 
+        _delay_ms(100); 
+    }
+
+    return 0;

e60e2ae9d680deab6fd6433a1765d47795e7853b
kelmerpassos <37123289+kelmerpassos@users.noreply.github.com>
2023-12-06 15:47:34 -0300

    Update sensor2.c

--- sensor2.c
+++ sensor2.c
@@ -13,15 +13,15 @@
 
 /*******************************************INITALIZE PORTS, TIMER, AND INTURRUPTS*******************************************/
 void init() {
-    DDRD = 0xFF;
+    DDRD |= (1<<DDD5);
     DDRC |= (1<<DDC4);							// Port C all output. PC0: RW		PC1: RS		PC2: E;
 	DDRC &= ~(1<<DDC5);						// Set Pin C5 as input to read Echo
 	PORTC |= (1<<PORTC5);					// Enable pull up on C5
 	PORTC &= ~(1<<PC4);						// Init C4 as low (trigger)
 
-	PRR &= ~(1<<PRTIM1);					// To activate timer1 module
+	PRR &= ~(1<<PRTIM1);					// To activate timer1 module;
 	TCNT1 = 0;								// Initial timer value
-	TCCR1B |= (1<<CS10);					// Timer without prescaller. Since default clock for atmega328p is 1Mhz period is 1uS
+	TCCR1B |= (1<<CS11);					// Timer without prescaller. Since default clock for atmega328p is 1Mhz period is 1uS
 	TCCR1B |= (1<<ICES1);					// First capture on rising edge
 
 	PCICR = (1<<PCIE1);						// Enable PCINT[14:8] we use pin C5 which is PCINT13
@@ -35,7 +35,7 @@ int main() {
 	while (1) {;
 		_delay_ms(60); 							// To allow sufficient time between queries (60ms min)
 		PORTC |= (1<<PC4);						// Set trigger high
-		_delay_us(15);							// for 10uS
+		_delay_us(10);							// for 10uS
 		PORTC &= ~(1<<PC4);						// to trigger the ultrasonic module
 	}
 }
@@ -44,15 +44,15 @@ ISR(PCINT1_vect) {
 	if (bit_is_set(PINC,PC5)) {					// Checks if echo is high
 		TCNT1 = 0;								// Reset Timer
 	} else {
-		uint16_t numuS = TCNT1;					// Save Timer value
+		long numuS = TCNT1/2;					// Save Timer value
 		uint8_t oldSREG = SREG;
 		cli();
         long distancia = numuS/58;
 		SREG = oldSREG;							// Enable interrupts
-        if (distancia < 100){
-            PORTD = 0xFF;
+        if (distancia < 30){
+            PORTD |= (1 << PORTD5);
         } else {
-            PORTD = 0x00;
+            PORTD &= ~(1 << PORTD5);
         }
 	}
 }

fe0d0bfff35fd660509caa46833cf3402a45e033
kelmerpassos <37123289+kelmerpassos@users.noreply.github.com>
2023-12-06 21:15:49 -0300

    Update sensor.c

--- sensor.c
+++ sensor.c
@@ -1,42 +1,48 @@
-/* 
- * File:   mainSensor.c
- * Author: ruanz
- *
- * Created on 24 de Novembro de 2023, 21:07
- */
-
 #define F_CPU 16000000UL
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
 
-#define TRIG_PIN PB0 // PINO 8 ARDUINO
-#define ECHO_PIN PB1 // PINO 9 ARDUINO
+void init() {
+    DDRD |= (1<<DDD5);
+    DDRC |= (1<<DDC4);
+	DDRC &= ~(1<<DDC5);
+	PORTC |= (1<<PORTC5);
+	PORTC &= ~(1<<PC4);
 
-long tempo, distancia;
+	PRR &= ~(1<<PRTIM1);
+	TCNT1 = 0;								
+	TCCR1B |= (1<<CS11);
+	TCCR1B |= (1<<ICES1);
 
-int main(void) {
-  DDRB |= (1 << TRIG_PIN); // PINO TRIG COMO OUTPUT
-  DDRB &= ~(1 << ECHO_PIN); // PINO ECHO COMO INPUT
-
-  PORTB &= ~((1 << TRIG_PIN) | (1 << ECHO_PIN)); // Inicia ECHO e TRIG como nível lógico baixo
-
-  TCCR1A = 0x00;
-  TCCR1B |= ((1 << CS11) & (1 << CS10)); // Ajusta o prescaler em 1:64 -> (64 / 16mHz) * 1m = 4ms
-  
-  while(1) {
-	// Envio do pulso de 10ms no pino TRIG
-	PORTB |= (1 << TRIG_PIN);
-	_delay_us(10);
-	PORTB &= ~(1 << TRIG_PIN);
-	
-	while((PINB & (1 << ECHO_PIN)) == 0); // Não avança enquanto o ECHO estiver em nível lógico baixo
-	TCNT1 = 0; // Inicia a contagem quando o ECHO sobre para nível lógico alto
-	while(PINB & (1 << ECHO_PIN)); // Não avança enquanto o ECHO estiver em nível lógico alto
-	
-	tempo = TCNT1 * 4.0; // Converte em microssegundos
-	distancia = tempo / 58.0; // Converte o tempo em distância em centímetros
-  }
+	PCICR = (1<<PCIE1);	
+	PCMSK1 = (1<<PCINT13);
+	sei();
 }
 
-
+int main() {
+	init();
+	while (1) {;
+		_delay_ms(60);						
+		PORTC |= (1<<PC4);
+		_delay_us(10);					
+		PORTC &= ~(1<<PC4);	
+	}
+}
+ISR(PCINT1_vect) {
+	if (bit_is_set(PINC,PC5)) {
+		TCNT1 = 0;		
+	} else {
+		long numuS = TCNT1/2;				
+		uint8_t oldSREG = SREG;
+		cli();
+        long distancia = numuS/58;
+		SREG = oldSREG;						
+        if (distancia < 30){
+            PORTD |= (1 << PORTD5);
+        } else {
+            PORTD &= ~(1 << PORTD5);
+        }
+	}
+}

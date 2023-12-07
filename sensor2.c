a85e39c0dd5c2797aa771613ceee4a9d0d60b1f5
Edilton-Damasceno <113069700+Edilton-Damasceno@users.noreply.github.com>
2023-12-06 21:03:53 -0300

    Traduzindo comentarios

--- sensor2.c
+++ sensor2.c
@@ -1,6 +1,6 @@
 /*
  *   
- *      Program uses a ATMega328p MCU to query a HC-SR04 ultrasonic module. 
+ *      Programa usando a ATMega328p MCU para consulta ao sensor ultrassonico HC-SR04. 
  *      Pin placement of ATMega328p:
  *      Pin PC4				HC-SR04 Trig
  *      Pin PC5				HC-SR04 Echo
@@ -11,44 +11,44 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
-/*******************************************INITALIZE PORTS, TIMER, AND INTURRUPTS*******************************************/
+/*******************************************INICIALIZANDO PORTAS, TEMPORIZADOR E INTERRUPÇÕES*******************************************/
 void init() {
-	DDRD |= (1<<DDD5);
-	DDRC |= (1<<DDC4);							// Port C all output. PC0: RW		PC1: RS		PC2: E;
-	DDRC &= ~(1<<DDC5);						// Set Pin C5 as input to read Echo
-	PORTC |= (1<<PORTC5);					// Enable pull up on C5
-	PORTC &= ~(1<<PC4);						// Init C4 as low (trigger)
+	DDRD |= (1<<DDD5);					// Define o pino D5 Como Saida
+	DDRC |= (1<<DDC4);					// Define o pino C4 Como Saida
+	DDRC &= ~(1<<DDC5);					// Define o pino C5 como entrada para ler o sinal do echo.
+	PORTC |= (1<<PORTC5);					// Habilitando o pull-up no pino C5.
+	PORTC &= ~(1<<PC4);					// Iniciando o pino C4 como baixo (Trig).
 
-	PRR &= ~(1<<PRTIM1);					// To activate timer1 module;
-	TCNT1 = 0;								// Initial timer value
-	TCCR1B |= (1<<CS11);					// Timer without prescaller. Since default clock for atmega328p is 1Mhz period is 1uS
-	TCCR1B |= (1<<ICES1);					// First capture on rising edge
+	PRR &= ~(1<<PRTIM1);					// Ativando o timer1;
+	TCNT1 = 0;						// Iniciando o valor do timer1
+	TCCR1B |= (1<<CS11);					// Habilitando o prescaller.
+	TCCR1B |= (1<<ICES1);					// Captura da borda de subida.
 
-	PCICR = (1<<PCIE1);						// Enable PCINT[14:8] we use pin C5 which is PCINT13
-	PCMSK1 = (1<<PCINT13);					// Enable C5 interrupt
-	sei();									// Enable Global Interrupts
+	PCICR = (1<<PCIE1);					// Habilitando o PCINT[14:8] usando o pino C5 que é o PCINY13.
+	PCMSK1 = (1<<PCINT13);					// Habilitando interrupção C5
+	sei();							// Habilitando interrupções globais
 }
 
-/*******************************************MAIN PROGRAM*******************************************/
+/*******************************************PROGRAMA PRINCIPAL*******************************************/
 int main() {
 	init();
 	while (1) {;
-		_delay_ms(60); 							// To allow sufficient time between queries (60ms min)
-		PORTC |= (1<<PC4);						// Set trigger high
-		_delay_us(10);							// for 10uS
-		PORTC &= ~(1<<PC4);						// to trigger the ultrasonic module
+		_delay_ms(60); 							// Para criar um atraso entre as consultas.
+		PORTC |= (1<<PC4);						// Definindo o Trig como alto.
+		_delay_us(10);							// Espera 10 microsegundos.
+		PORTC &= ~(1<<PC4);						// Volta o pino Trig para nivel baixo.
 	}
 }
-/*******************************************INTURRUPT PCINT1 FOR PIN C5*******************************************/
+/*******************************************INTURRUPÇÃO PCINT1 PARA O PINO C5*******************************************/
 ISR(PCINT1_vect) {
-	if (bit_is_set(PINC,PC5)) {	// Checks if echo is high
-		TCNT1 = 0;								// Reset Timer
+	if (bit_is_set(PINC,PC5)) {	  					// Checando se o pino echo está em estado alto.
+		TCNT1 = 0;							// Zerando o timer.
 	} else {
-		long numuS = TCNT1/2;					// Save Timer value
+		long numuS = TCNT1/2;						// Salvando o valor do timer.
 		uint8_t oldSREG = SREG;
 		cli();
 		long distancia = numuS/58;
-		SREG = oldSREG;							// Enable interrupts
+		SREG = oldSREG;							// Habilitando interrupções.
 		if (distancia < 30){
 				PORTD |= (1 << PORTD5);
 		} else {
